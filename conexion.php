3427a93d1440adea58a2eb495d54cd4c092fb6a1
yovic-21 <ypahuarac@autonoma.edu.pe>
2025-10-26 20:02:48 -0500

    nuevo conexion

--- conexion.php
+++ conexion.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "localhost";  
-$username = "root";         
-$password = "";             
-$database = "habla_pet";    
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$usuario = "ebfe99f5782b800084ba2feb57ce";
+$contraseña = "068febfe-99f5-79ec-8000-b5a53f9f318b";
+$base_de_datos = "habla_pet";
+$puerto = 10272;  // Muy importante
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($host, $usuario, $contraseña, $base_de_datos, $puerto);
 
 if ($conn->connect_error) {
     die("❌ Error de conexión: " . $conn->connect_error);
